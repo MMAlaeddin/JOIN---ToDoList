b87a93cf96edd6ac3cc2a08540a603b755236790
Mariam Alaeddin <mariamalaeddin@gmail.com>
2020-03-24 12:14:14 -0700

    add authorization requirements for user
    > Co-authored-by: Benjamin Thom <jaminthom@gmail.com>
    > > Co-authored-by: Rachel Schieferstein <violena@gmail.com>
    > > Co-authored-by: Joseph <45412175+fractalscape13@users.noreply.github.com>

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,17 +1,22 @@
 using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
 
 
 namespace ToDoList.Models
 {
   public class Item
   {
-
     public Item()
-      {
-          this.Categories = new HashSet<CategoryItem>();
-      }
+    {
+      this.Categories = new HashSet<CategoryItem>();
+    }
     public int ItemId { get; set; }
     public string Description { get; set; }
+    public virtual ApplicationUser User { get; set; }
+
     public ICollection<CategoryItem> Categories { get; }
   }
 }
